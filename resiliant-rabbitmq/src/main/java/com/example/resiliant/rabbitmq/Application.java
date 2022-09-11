bb05f742710c5409cb1a564764e622b19c34c68e
Hazar Gul Nazari <nazariazargul@gmail.com>
2022-09-11 21:36:19 +0200

    add all

--- resiliant-rabbitmq/src/main/java/com/example/resiliant/rabbitmq/Application.java
+++ resiliant-rabbitmq/src/main/java/com/example/resiliant/rabbitmq/Application.java
@@ -2,11 +2,17 @@ package com.example.resiliant.rabbitmq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RestController
@@ -24,13 +30,13 @@ public class Application {
 	@GetMapping()
 	public void publish(){
 		try{
-//			if(rabbitMqConfigurator.rabbitTemplate().send()){
-//				System.out.println("Yes, the rabbitmq is running");
-//			}
-//			else System.out.println("No, the rabbitmq is not running");
-			rabbitMqConfigurator.rabbitTemplate().convertAndSend("amq.atopic","", "Hello world");
+			RabbitTemplate rabbitTemplate = rabbitMqConfigurator.rabbitTemplate();
+			Message message = new Message("Hello world".getBytes(StandardCharsets.UTF_8));
+			rabbitTemplate.send(message);
+			rabbitTemplate.convertAndSend("amq.atopic","", "Hello world");
 		}
 		catch(Exception ex){
+			System.out.println("Something bad happened");
 			LOGGER.error("Sorry, the rabbitmq connection has issue. Temporarily, we are storing your message locally");
 		}
 	}
--- resiliant-rabbitmq/src/main/java/com/example/resiliant/rabbitmq/RabbitMqConfigurator.java
+++ resiliant-rabbitmq/src/main/java/com/example/resiliant/rabbitmq/RabbitMqConfigurator.java
@@ -1,6 +1,9 @@
 package com.example.resiliant.rabbitmq;
 
+import com.rabbitmq.client.ShutdownSignalException;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.Connection;
+import org.springframework.amqp.rabbit.connection.ConnectionListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
